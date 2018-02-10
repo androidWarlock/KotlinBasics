889310d9cd90c33856a0a4e526c586b257e8cbf0
zMabrook <boghdady107@gmail.com>
2018-02-10 22:46:42 +0200

    kotlin basics Examples

--- src/oop/inheritanceclasses.kt
+++ src/oop/inheritanceclasses.kt
@@ -1,2 +1,31 @@
 package oop
 
+abstract class Vechile{
+    abstract fun drive()
+}
+
+class Bycicle : Vechile(){
+    override fun drive() {
+        "driving bycicle"
+    }
+    fun bycicle() ="hello"
+
+}
+
+
+class Boat : Vechile(){
+    override fun drive() {
+        "driving boat"
+    }
+
+    fun boat() ="hello"
+}
+
+fun main(args: Array<String>) {
+    val vechile: Vechile = Bycicle()
+    vechile.drive()
+
+    if (vechile is Bycicle ){
+        vechile.bycicle()
+    }
+}
